571bd0fbbc80875b15b03c5b1f288b75946fb210
marselleze <146928013+marselleze@users.noreply.github.com>
2024-07-06 18:17:43 +0300

    code: добавлен вывод произведения a и b

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
     cin >> a >> b;
 
     cout << "A + B = " << a + b << '\n'
-         << "A - B = " << a - b << '\n';
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n';
 }
