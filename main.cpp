3baf7478ebd87f40c2ba762f765e2e9e2a6ad4e0
marselleze <146928013+marselleze@users.noreply.github.com>
2024-07-06 18:32:13 +0300

    code: добавлен вывод минимума из a и b

--- main.cpp
+++ main.cpp
@@ -11,5 +11,6 @@ int main() {
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n'
-         << "max(a, b) = " << max(a, b) << '\n';
+         << "max(a, b) = " << max(a, b) << '\n'
+         << "min(a, b) = " << min(a, b) << "\n";
 }
